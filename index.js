5574a5c98e33e61c92a605117babf80a537b2cb7
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2024-03-04 00:46:11 +0530

    Added signups

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
 const authenticate = require('./middleware/authenticate');
 const { JWT_SECRET } = require('./config');
+const signupRouter = require('./routes/signup');
+const profileRouter = require('./routes/profile');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = 'mongodb+srv://Ayush:2BXNWx4qaZkr3C5y@cluster0.ijs1ymf.mongodb.net/jwt?retryWrites=true&w=majority&appName=Cluster0';
 
+
 app.use(express.json());
 app.use('/auth', authRoutes);
+app.use('/auth', signupRouter);
+app.use('/auth', profileRouter);
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,25 +1,5 @@
-// const jwt = require('jsonwebtoken');
-// const { JWT_SECRET } = require('../config');
-
-// const authenticate = (req, res, next) => {
-//   const token = req.header('Authorization');
-//   if (!token) return res.status(401).json({ message: 'Authorization header missing' });
-
-//   try {
-//     const decoded = jwt.verify(token, JWT_SECRET);
-//     req.user = decoded;
-//     next();
-//   } catch (error) {
-//     console.error(error);
-//     res.status(401).json({ message: 'Invalid token' });
-//   }
-// };
-
-// module.exports = authenticate;
-
-
 const jwt = require('jsonwebtoken');
-const User = require('./models/User');
+const User = require('../models/User');
 
 const authenticate = async (req, res, next) => {
   // Get the JWT token from the Authorization header
@@ -35,11 +15,13 @@ const authenticate = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     // Extract user ID from decoded token
-    const userId = decoded.userId;
-
+    const username = decoded.username;
+    
     // Fetch user from database based on user ID
-    const user = await User.findById(userId);
-
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: 'Unauthorized: User not found' });
+    }
     // Attach user object to request for use in subsequent middleware or routes
     req.user = user;
 
